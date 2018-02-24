d09a8f73d22cd2f08b384c63e5f83bec1a4455b3
Jonas Mortensen <Jonasmortensen3@gmail.com>
2018-02-24 15:28:53 +0100

    Seperated cam in three modes axis

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -18,10 +18,14 @@ public class CameraController : MonoBehaviour {
 
     private Camera mainCam;
     private CameraViewMode viewMode;
+    private CameraRotationMode rotationMode;
+    private CameraMovementnMode movementMode;
 
     private Vector3 goalPos;
     private Vector3 startPos;
-    private bool inTransition;
+    private Quaternion startRot;
+    private Quaternion goalRot;
+    public bool inTransition;
     private float elapsedTime;
     private float transitionTime;
 
@@ -42,35 +46,33 @@ public class CameraController : MonoBehaviour {
             float t = elapsedTime / transitionTime;
             t = t * t * t * (t * (6f * t - 15f) + 10f);
             mainCam.transform.position = Vector3.Lerp(startPos, goalPos, t);
+            mainCam.transform.localRotation = Quaternion.Lerp(startRot, goalRot, t);
+
             if (elapsedTime > transitionTime) {
                 inTransition = false;
             }
         }
 
-        mainCam.transform.LookAt(transform.position);
+        //mainCam.transform.LookAt(transform.position);
     }
 
-    public void SetMovementMode(CameraViewMode movementMode) {
-        switch (movementMode) {
+    public void SetViewMode(CameraViewMode _viewMode) {
+        SetCameraMode(_viewMode, rotationMode, movementMode);
+    }
+
+    public void SetCameraMode(CameraViewMode _viewMode, CameraRotationMode _rotationMode, CameraMovementnMode _movementMode) {
+        switch (_viewMode) {
             case CameraViewMode.FRONT:
                 goalPos = transform.position - (transform.forward * 10) + (transform.up * 5);
+                goalRot = Quaternion.Euler(new Vector3(10, 0, 0));
                 break;
             case CameraViewMode.TOP:
                 goalPos = transform.position + (transform.up * 10);
-                break;
-        }
-    }
-
-    public void SetMovementMode(CameraViewMode viewMode, CameraRotationMode rotationMode, CameraMovementnMode movementMode) {
-        switch (viewMode) {
-            case CameraViewMode.FRONT:
-                goalPos = transform.position - (transform.forward * 10) + (transform.up * 5);
-                break;
-            case CameraViewMode.TOP:
-                goalPos = transform.position + (transform.up * 10);
+                goalRot = Quaternion.Euler(new Vector3(90, 0, 0));
                 break;
             case CameraViewMode.LEFT:
-                goalPos = transform.position - (transform.right * 10 + transform.up * 5);
+                goalPos = transform.position - (transform.right * 10) + transform.up * 5;
+                goalRot = Quaternion.Euler(new Vector3(10, 90, 0));
                 break;
         }
     }
@@ -83,9 +85,13 @@ public class CameraController : MonoBehaviour {
     }
 
     public void UpdatePosition(float time) {
+        startPos = mainCam.transform.position;
+        startRot = mainCam.transform.localRotation;
+        if (Vector3.Distance(startPos, goalPos) < 0.01f) {
+            return;
+        }
         elapsedTime = 0;
         transitionTime = time;
-        startPos = mainCam.transform.position;
         inTransition = true;
     }
 }
--- Assets/Scripts/Input/KeyboardInput.cs
+++ Assets/Scripts/Input/KeyboardInput.cs
@@ -14,11 +14,15 @@ public class KeyboardInput : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Q)) {
-            camController.SetMovementMode(CameraViewMode.FRONT);
+            camController.SetViewMode(CameraViewMode.FRONT);
             camController.UpdatePosition(transitionTime);
         }
         if (Input.GetKeyDown(KeyCode.W)) {
-            camController.SetMovementMode(CameraViewMode.TOP);
+            camController.SetViewMode(CameraViewMode.TOP);
+            camController.UpdatePosition(transitionTime);
+        }
+        if (Input.GetKeyDown(KeyCode.E)) {
+            camController.SetViewMode(CameraViewMode.LEFT);
             camController.UpdatePosition(transitionTime);
         }
     }
