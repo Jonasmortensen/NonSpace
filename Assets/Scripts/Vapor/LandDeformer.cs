72037b14bd5d1125ba2c3daa4cb6ffafb1e9454c
Jonas Mortensen <Jonasmortensen3@gmail.com>
2018-03-04 15:33:47 +0100

    Setup as sliders and fixed animation

--- Assets/Scripts/Vapor/LandDeformer.cs
+++ Assets/Scripts/Vapor/LandDeformer.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class LandDeformer : MonoBehaviour {
 
     public Texture2D noiseTexture;
-    public float height;
-    public float scale;
+    private float height;
+    private float scale = 0.5f;
     public AnimationCurve valley;
-    public float waveAmount;
-    public float waveSpeed;
+    [Range(0, 50)]
+    public float heightSlider;
+    [Range(0, 1)]
+    public float movementSlider;
+    private float waveAmount;
+    private float waveSpeed;
+    private float offsetPrSecond;
     private float minX;
     private float maxX;
+    private float yOffset;
 
     private MeshFilter meshFilter;
 
@@ -52,19 +58,35 @@ public class LandDeformer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        SetHeight(heightSlider);
+        SetMovement(movementSlider);
         UpdateMesh();
+    }
+
+    public void SetHeight(float _height) {
+        height = _height;
+    }
+
+    public void SetMovement(float movement) {
+        waveAmount = 0.3f * movement;
+        offsetPrSecond = 0.5f * movement;
     }
 
     void UpdateMesh() {
         Mesh sourceMesh = meshFilter.mesh;
+        yOffset += offsetPrSecond * Time.deltaTime;
         
         Vector3[] vertices = sourceMesh.vertices;
         for (int i = 0; i < vertices.Length; i++) {
             Vector3 vertex = vertices[i];
             float t = (vertex.x - minX) / (maxX - minX);
-            float textureSample = noiseTexture.GetPixel((int)(vertex.x * scale), (int)(vertex.y * scale)).r;
+
+            //float textureSample = noiseTexture.GetPixel((int)(vertex.x * scale) % noiseTexture.width, (int)(vertex.y * scale + yOffset) % noiseTexture.height).r;
+            float textureSample = Mathf.PerlinNoise((vertex.x * scale), (vertex.y * scale + yOffset));
+
+
             float sineValue = Mathf.Sin(vertex.x + Time.time * waveSpeed);
-            vertex.z = (textureSample * height + sineValue * waveAmount) * valley.Evaluate(t);
+            vertex.z = (textureSample * height + sineValue * waveAmount * height) * valley.Evaluate(t);
             vertices[i] = vertex;
         }
         
